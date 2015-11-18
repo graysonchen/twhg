c4489aa5f93c2f2eda195bddb2476ddf04ba7805
Grayson Chen <cgg5207@sina.com>
2015-11-18 22:07:25 +0800

    使用git filter-branch删除真实的 trello key https://help.github.com/articles/remove-sensitive-data/

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:cgg5207/twhg.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push( 'config/database.yml','config/secrets.yml', 'Gemfile.lock')
+set :linked_files, fetch(:linked_files, []).push( 'config/database.yml','config/secrets.yml', 'config/config.yml', 'Gemfile.lock')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/cache','vendor/bundle', 'public/system')
